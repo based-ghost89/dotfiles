7d890e401e4d9f8bb26a117b1a8372aeb95ebd52
based-ghost89 <mrangelags@gmail.com>
2022-06-04 01:06:58 -0500

    added new file vm-tools.sh

---
+++ vm-tools.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+mkdir ~/.config/alacritty &&
+  cp ~/usr/share/doc/alacritty/doc/alacritty.yml ~/.config/alacritty/alacritty.yml
+mkdir ~/.config/tmux &&
+  cp ~/repos/dotfiles/tmux.conf ~/.config/tmux/tmux.conf
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/SpaceMono.zip
