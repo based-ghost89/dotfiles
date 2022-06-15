c5d3dcb63f082fc0d110415a3841001f70776d1e
based-ghost89 <mrangelags@gmail.com>
2022-06-14 20:49:57 -0500

    added new files vm-notes

--- vm-tools.sh
+++ vm-tools.sh
@@ -4,3 +4,29 @@ mkdir ~/.config/alacritty &&
 mkdir ~/.config/tmux &&
   cp ~/repos/dotfiles/tmux.conf ~/.config/tmux/tmux.conf
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/SpaceMono.zip
+#Archlinux make-depends
+sudo pacman -S base-devel
+autoconf
+automake
+binutils
+bison
+esysusers
+etmpfiles
+fakeroot
+findutils
+flex
+gawk
+gcc
+gettext
+grep
+gzip
+libmnl
+libtool
+m4
+patch
+sed
+texinfo
+which
+
+#Debian make-depends
+#OpenSUSE make-depends
